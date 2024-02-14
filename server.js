6be54c78f179a2c13ef1ac60450f529470a8a967
codec404 <saptarshimemari072@gmail.com>
2024-02-14 23:32:37 +0530

    Authentication--begin :)

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
-import {connectDb} from "./config/db.js";
+import { connectDb } from "./config/db.js";
 import morgan from "morgan";
+import "./config/oauth.js";
+import passport from "passport";
 
 dotenv.config();
 connectDb();
@@ -13,8 +15,19 @@ const app = express();
 //MIDDLEWARES
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
+
+// GOOGLE LOGIN
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
